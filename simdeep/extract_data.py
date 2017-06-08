add CV + fix normalization

simdeep/extract_data.py
@@ -9,17 +9,16 @@ from simdeep.config import SURVIVAL_TSV
 from simdeep.config import SURVIVAL_TSV_TEST
 from simdeep.config import PATH_DATA
 
-from simdeep.config import MAD_SCALE
-from simdeep.config import ROBUST_SCALE
-from simdeep.config import MIN_MAX_SCALE
-from simdeep.config import UNIT_NORM
-from simdeep.config import RANK_SCALE
-from simdeep.config import CORRELATION_REDUCER
-
 from simdeep.config import TRAIN_MIN_MAX
 from simdeep.config import TRAIN_NORM_SCALE
-from simdeep.config import TRAIN_DIM_REDUCTION
+from simdeep.config import TRAIN_CORR_REDUCTION
 from simdeep.config import TRAIN_RANK_NORM
+from simdeep.config import TRAIN_MAD_SCALE
+from simdeep.config import TRAIN_ROBUST_SCALE
+from simdeep.config import TRAIN_CORR_RANK_NORM
+
+from simdeep.config import CROSS_VALIDATION_INSTANCE
+from simdeep.config import TEST_FOLD
 
 from simdeep.survival_utils import load_data_from_tsv
 from simdeep.survival_utils import load_survival_file
@@ -29,8 +28,6 @@ from simdeep.survival_utils import CorrelationReducer
 
 from time import time
 
-from numpy import hstack
-
 import numpy as np
 
 
@@ -38,14 +35,16 @@ def main():
     """ """
     load_data = LoadData()
     load_data.load_array()
+    load_data.load_survival()
+    load_data.create_a_cv_split()
+
     load_data.normalize_training_array()
 
-    load_data.load_survival()
+    load_data.load_matrix_test_fold()
 
     load_data.load_matrix_test()
-    # load_data.reorder_test_matrix('SIJIA')
-
     load_data.load_survival_test()
+
 
 class LoadData():
     def __init__(
@@ -54,7 +53,10 @@ class LoadData():
             training_tsv=TRAINING_TSV,
             test_tsv=TEST_TSV,
             survival_tsv=SURVIVAL_TSV,
-            survival_tsv_test=SURVIVAL_TSV_TEST):
+            survival_tsv_test=SURVIVAL_TSV_TEST,
+            cross_validation_instance=CROSS_VALIDATION_INSTANCE,
+            test_fold=TEST_FOLD,
+    ):
         """
         class to extract data
         :training_matrices: dict(matrice_type, path to the tsv file)
@@ -82,17 +84,25 @@ class LoadData():
         self.sample_ids = []
         self.data_type = training_tsv.keys()
 
-        self.matrix_stacked = None
-        self.features_stacked = None
         self.survival = None
 
         self.survival_tsv_test = survival_tsv_test
 
         self.feature_test_array = {}
         self.matrix_test_array = {}
+
+        self.sample_ids_cv = []
+        self.matrix_cv_array = {}
+        self.survival_cv = None
+
         self.matrix_ref_array = {}
+        self.feature_ref_array = {}
+
         self.survival_test = None
         self.sample_ids_test = None
+
+        self.cross_validation_instance = cross_validation_instance
+        self.test_fold = test_fold
 
         self.do_feature_reduction = None
 
@@ -102,9 +112,31 @@ class LoadData():
         self.min_max_scaler = MinMaxScaler()
         self.dim_reducer = CorrelationReducer()
 
+    def load_matrix_test_fold(self):
+        """ """
+        for key in self.test_tsv:
+
+            matrix_test = self.matrix_cv_array[key].copy()
+            matrix_ref = self.matrix_array[key].copy()
+
+            matrix_ref, matrix_test = self.transform_matrices(
+                matrix_ref, matrix_test, key,
+                unit_norm=TRAIN_NORM_SCALE,
+                rank_scale=TRAIN_RANK_NORM,
+                mad_scale=TRAIN_MAD_SCALE,
+                robust_scale=TRAIN_ROBUST_SCALE,
+                min_max_scale=TRAIN_MIN_MAX,
+                correlation_reducer=TRAIN_CORR_REDUCTION,
+                corr_rank_scale=TRAIN_CORR_RANK_NORM,
+            )
+
+            self.matrix_cv_array[key] = matrix_test
+            self.feature_ref_array[key] = self.feature_array[key][:]
+
     def load_matrix_test(self):
         """ """
         for key in self.test_tsv:
+            self.feature_ref_array[key] = self.feature_array[key][:]
             sample_ids, feature_ids, matrix = load_data_from_tsv(self.test_tsv[key],
                                                              path_data=self.path_data)
 
@@ -133,8 +165,11 @@ class LoadData():
                 matrix_ref, matrix_test, key,
                 unit_norm=TRAIN_NORM_SCALE,
                 rank_scale=TRAIN_RANK_NORM,
+                mad_scale=TRAIN_MAD_SCALE,
+                robust_scale=TRAIN_ROBUST_SCALE,
                 min_max_scale=TRAIN_MIN_MAX,
-                correlation_reducer=TRAIN_DIM_REDUCTION,
+                correlation_reducer=TRAIN_CORR_REDUCTION,
+                corr_rank_scale=TRAIN_CORR_RANK_NORM,
             )
 
             self.matrix_test_array[key] = matrix_test
@@ -143,7 +178,7 @@ class LoadData():
     def reorder_test_matrix(self, key):
         """ """
         features_test = self.feature_test_array[key]
-        features_ref = self.feature_array[key]
+        features_ref = self.feature_ref_array[key]
 
         ref_dict = {feat: pos for pos, feat in enumerate(features_test)}
         index = [ref_dict[feat] for feat in features_ref]
@@ -182,15 +217,25 @@ class LoadData():
 
             print('{0} loaded of dim:{1}'.format(f_name, matrix.shape))
 
-        self._stack_matrices()
         print('data loaded in {0} s'.format(time() - t))
 
-    def _stack_matrices(self):
+    def create_a_cv_split(self):
         """ """
-        self.matrix_stacked = hstack([self.matrix_array[data]
-                                      for data in self.data_type])
-        self.features_stacked = [feat for data in self.data_type
-                                 for feat in self.feature_array[data]]
+        if not self.cross_validation_instance:
+            return
+
+        cv = self.cross_validation_instance
+        train, test = [(tn, tt) for tn, tt in cv.split(self.sample_ids)][self.test_fold]
+
+        for key in self.matrix_array:
+            self.matrix_cv_array[key] = self.matrix_array[key][test]
+            self.matrix_array[key] = self.matrix_array[key][train]
+
+        self.survival_test = self.survival.copy()[test]
+        self.survival = self.survival[train]
+
+        self.sample_ids_cv = np.asarray(self.sample_ids)[test].tolist()
+        self.sample_ids = np.asarray(self.sample_ids)[train].tolist()
 
     def load_survival(self):
         """ """
@@ -198,7 +243,11 @@ class LoadData():
         matrix = []
 
         for sample in self.sample_ids:
-            assert(sample in survival)
+            try:
+                assert(sample in survival)
+            except AssertionError:
+                raise Exception('sample: {0} not in survival!'.format(sample))
+
             matrix.append(survival[sample])
 
         self.survival = np.asmatrix(matrix)
@@ -209,7 +258,11 @@ class LoadData():
         matrix = []
 
         for sample in self.sample_ids_test:
-            assert(sample in survival)
+            try:
+                assert(sample in survival)
+            except AssertionError:
+                raise Exception('sample: {0} not in survival!'.format(sample))
+
             matrix.append(survival[sample])
 
         self.survival_test = np.asmatrix(matrix)
@@ -224,32 +277,41 @@ class LoadData():
     def _normalize(self,
                    matrix,
                    key,
-                   do_min_max=TRAIN_MIN_MAX,
-                   do_norm_scale=TRAIN_NORM_SCALE,
-                   do_rank_scale=TRAIN_RANK_NORM,
-                   do_dim_reduction=TRAIN_DIM_REDUCTION):
+                   mad_scale=TRAIN_MAD_SCALE,
+                   robust_scale=TRAIN_ROBUST_SCALE,
+                   min_max=TRAIN_MIN_MAX,
+                   norm_scale=TRAIN_NORM_SCALE,
+                   rank_scale=TRAIN_RANK_NORM,
+                   corr_rank_scale=TRAIN_CORR_RANK_NORM,
+                   dim_reduction=TRAIN_CORR_REDUCTION):
         """ """
-        print('normalizing...')
+        print('normalizing for {0}...'.format(key))
 
-        if do_min_max:
+        if mad_scale:
+            matrix = self.mad_scaler.fit_transform(matrix.T).T
+
+        if robust_scale:
+            matrix = self.robust_scaler.fit_transform(matrix)
+
+        if min_max:
             matrix = MinMaxScaler().fit_transform(
-                matrix)
+                matrix.T).T
 
-        if do_rank_scale and not do_min_max:
+        if rank_scale:
             matrix = RankNorm().fit_transform(
                 matrix)
 
-        if do_dim_reduction:
-            print('dim reduction...')
+        if dim_reduction:
+            print('dim reduction for {0}...'.format(key))
             reducer = CorrelationReducer()
             matrix = reducer.fit_transform(
                 matrix)
 
-            if do_rank_scale:
+            if corr_rank_scale:
                 matrix = RankNorm().fit_transform(
                     matrix)
 
-        if do_norm_scale:
+        if norm_scale:
             matrix = self.normalizer.fit_transform(
                 matrix)
 
@@ -257,33 +319,22 @@ class LoadData():
 
     def transform_matrices(self,
                            matrix_ref, matrix, key,
-                           mad_scale=MAD_SCALE,
-                           robust_scale=ROBUST_SCALE,
-                           min_max_scale=MIN_MAX_SCALE,
-                           rank_scale=RANK_SCALE,
-                           correlation_reducer=CORRELATION_REDUCER,
-                           unit_norm=UNIT_NORM):
+                           mad_scale=TRAIN_MAD_SCALE,
+                           robust_scale=TRAIN_ROBUST_SCALE,
+                           min_max_scale=TRAIN_MIN_MAX,
+                           rank_scale=TRAIN_RANK_NORM,
+                           correlation_reducer=TRAIN_CORR_REDUCTION,
+                           corr_rank_scale=TRAIN_CORR_RANK_NORM,
+                           unit_norm=TRAIN_NORM_SCALE):
         """ """
         print('Scaling/Normalising dataset...')
         if min_max_scale:
-            matrix_ref = self.min_max_scaler.fit_transform(matrix_ref)
-            matrix = self.min_max_scaler.fit_transform(matrix)
+            matrix_ref = self.min_max_scaler.fit_transform(matrix_ref.T).T
+            matrix = self.min_max_scaler.fit_transform(matrix.T).T
 
         if rank_scale and not min_max_scale:
             matrix_ref = RankNorm().fit_transform(matrix_ref)
             matrix = RankNorm().fit_transform(matrix)
-
-        if correlation_reducer:
-            reducer = CorrelationReducer()
-            matrix_ref = reducer.fit_transform(matrix_ref)
-            matrix = reducer.transform(matrix)
-
-            self.feature_test_array[key] = self.sample_ids
-            self.feature_array[key] = self.sample_ids
-
-            if rank_scale:
-                matrix_ref = RankNorm().fit_transform(matrix_ref)
-                matrix = RankNorm().fit_transform(matrix)
 
         if mad_scale:
             matrix_ref = self.mad_scaler.fit_transform(matrix_ref.T).T
@@ -292,6 +343,18 @@ class LoadData():
         if robust_scale:
             matrix_ref = self.robust_scaler.fit_transform(matrix_ref)
             matrix = self.robust_scaler.transform(matrix)
+
+        if correlation_reducer:
+            reducer = CorrelationReducer()
+            matrix_ref = reducer.fit_transform(matrix_ref)
+            matrix = reducer.transform(matrix)
+
+            self.feature_test_array[key] = self.sample_ids
+            self.feature_ref_array[key] = self.sample_ids
+
+            if corr_rank_scale:
+                matrix_ref = RankNorm().fit_transform(matrix_ref)
+                matrix = RankNorm().fit_transform(matrix)
 
         if unit_norm:
             matrix_ref = self.normalizer.fit_transform(matrix_ref)

simdeep/simdeep_analysis.py
@@ -11,15 +11,9 @@ from simdeep.deepmodel_base import DeepBase
 from simdeep.config import NB_CLUSTERS
 from simdeep.config import CLUSTER_ARRAY
 from simdeep.config import PVALUE_THRESHOLD
-from simdeep.config import SELECT_FEATURES_METHOD
+from simdeep.config import CLASSIFIER_TYPE
 from simdeep.config import CLASSIFIER
 from simdeep.config import MIXTURE_PARAMS
-
-from simdeep.config import MAD_SCALE
-from simdeep.config import ROBUST_SCALE
-from simdeep.config import MIN_MAX_SCALE
-from simdeep.config import UNIT_NORM
-from simdeep.config import RANK_SCALE
 
 from simdeep.config import CLUSTER_EVAL_METHOD
 from simdeep.config import CLUSTER_METHOD
@@ -47,7 +41,7 @@ def main():
     sim_deep.load_training_dataset()
     sim_deep.fit()
     sim_deep.load_test_dataset()
-    sim_deep.predict_labels_on_test_dataset_v2()
+    sim_deep.predict_labels_on_test_dataset()
     # sim_deep.predict_labels_on_test_dataset()
 
 
@@ -58,6 +52,7 @@ class SimDeep(DeepBase):
                  pvalue_thres=PVALUE_THRESHOLD,
                  cluster_method=CLUSTER_METHOD,
                  cluster_eval_method=CLUSTER_EVAL_METHOD,
+                 classifier_type=CLASSIFIER_TYPE,
                  **kwargs):
         """
         ### AUTOENCODER PARAMETERS ###:
@@ -83,6 +78,7 @@ class SimDeep(DeepBase):
         self.pvalue_thres = pvalue_thres
         self.classifier_grid = CLASSIFIER
         self.classifier = None
+        self.classifier_type = classifier_type
 
         self.valid_node_ids_array = {}
         self.activities_array = {}
@@ -109,72 +105,54 @@ class SimDeep(DeepBase):
         """
         self.construct_autoencoders()
         self.look_for_survival_nodes()
-        labels = self.predict_labels()
-        self.labels = labels
+        self.predict_labels()
+        self.fit_classification_model()
 
-    def predict_labels_on_test_dataset(
-            self,
-            return_proba=False,
-            mad_scale=MAD_SCALE,
-            robust_scale=ROBUST_SCALE,
-            min_max_scale=MIN_MAX_SCALE,
-            unit_norm=UNIT_NORM,
-            rank_scale=RANK_SCALE):
+    def predict_labels_on_test_fold(self):
         """
-        predict labels of the test set
-
-        argument:
-            :return_proba: Bool    if true return class probabilities instead
-            :mad_scale: Bool (default False)    use the mad scaler normalisation
-            robust_scale: Bool (default False)    use the robust scale transformation
-            min_max_scale: Bool (default False)    use min max transformation
-            unit_norm: Bool (default False)    use the unit norm normalization
-
-        return:
-            class labels (array), Cox-PH P-value
         """
-        test_matrix = self.dataset.matrix_test
-        ref_matrix = self.dataset.matrix_ref
+        # self.dataset.reorder_matrix_test()
 
-        ref_matrix, test_matrix = self.dataset.transform_matrices(
-            ref_matrix,
-            test_matrix,
-            mad_scale=mad_scale,
-            robust_scale=robust_scale,
-            min_max_scale=min_max_scale,
-            unit_norm=unit_norm,
-            rank_scale=rank_scale,
-        )
+        self.dataset.load_matrix_test_fold()
 
-        data_type = self.dataset.data_type_test
-        nbdays, isdead = self.dataset.survival_test.T.tolist()
+        nbdays, isdead = self.dataset.survival_cv.T.tolist()
+        activities_array = []
 
-        print('feature selection...')
-        embbed = SELECT_FEATURES_METHOD[data_type]
+        for key in self.training_omic_list:
 
-        train_matrix = embbed.fit_transform(ref_matrix, self.labels)
-        test_matrix = embbed.transform(test_matrix)
+            test_matrix = self.dataset.matrix_cv_array[key]
+            encoder = self.encoder_array[key]
+            valid_node_ids = self.valid_node_ids_array[key]
 
-        self._do_classification(train_matrix, test_matrix, self.labels)
+            activities_array.append(encoder.predict(test_matrix).T[valid_node_ids].T)
+
+        self.activities_test = hstack(activities_array)
+
+        self._predict_test_labels()
+
+        self.test_labels = [self._label_ordered_dict[label]
+                            for label in self.test_labels[:]]
+
+        print('#### report of assigned cluster:')
+        for key, value in Counter(self.test_labels).items():
+            print('class: {0}, number of samples :{1}'.format(key, value))
 
         pvalue = coxph(self.test_labels, isdead, nbdays)
 
         print('Cox-PH p-value (Log-Rank) for inferred labels: {0}'.format(pvalue))
 
-        labels = self.test_labels_proba if return_proba else self.test_labels
+        return self.test_labels, pvalue
 
-        return labels, pvalue
-
-    def predict_labels_on_test_dataset_v2(self, predict_proba=False):
+    def predict_labels_on_test_dataset(self):
         """
         """
         # self.dataset.reorder_matrix_test()
 
         nbdays, isdead = self.dataset.survival_test.T.tolist()
 
-        if self.dataset.test_tsv.keys() != self.training_omic_list:
+        if set(self.dataset.test_tsv.keys()) != set(self.training_omic_list):
             self.look_for_survival_nodes(self.dataset.test_tsv.keys())
-            labels = self.predict_labels()
+            self.fit_classification_model()
 
         activities_array = []
 
@@ -187,15 +165,12 @@ class SimDeep(DeepBase):
 
             activities_array.append(encoder.predict(test_matrix).T[valid_node_ids].T)
 
-        activities_stacked = hstack(activities_array)
+        self.activities_test = hstack(activities_array)
 
-        test_labels = self.clustering.predict(activities_stacked)
+        self._predict_test_labels()
 
         self.test_labels = [self._label_ordered_dict[label]
-                            for label in test_labels]
-
-        if predict_proba:
-            self.test_labels_proba = self.clustering.predict_proba(activities_stacked)
+                            for label in self.test_labels[:]]
 
         print('#### report of assigned cluster:')
         for key, value in Counter(self.test_labels).items():
@@ -205,12 +180,18 @@ class SimDeep(DeepBase):
 
         print('Cox-PH p-value (Log-Rank) for inferred labels: {0}'.format(pvalue))
 
-        labels = self.test_labels
+        return self.test_labels, pvalue
 
-        return labels, pvalue
-
-    def _do_classification(self, train_matrix, test_matrix, labels):
+    def fit_classification_model(self):
         """ """
+        train_matrix = self.activities_train
+        labels = self.labels
+
+        if self.classifier_type == 'clustering':
+            print('clustering model defined as the classifier')
+            self.classifier = self.clustering
+            return
+
         print('classification analysis...')
 
         self.classifier_grid.fit(train_matrix, labels)
@@ -221,15 +202,82 @@ class SimDeep(DeepBase):
         print('cross val score: {0}'.format(np.mean(cvs)))
         self.classifier.set_params(probability=True)
         self.classifier.fit(train_matrix, labels)
-
         print('classification score:', self.classifier.score(train_matrix, labels))
 
-        self.test_labels = self.classifier.predict(test_matrix)
-        self.test_labels_proba = self.classifier.predict_proba(test_matrix)
+    def predict_labels(self):
+        """
+        predict labels from training set
+        using K-Means algorithm on the node activities,
+        using only nodes linked to survival
+        """
+        print('performing clustering on the omic model with the following key:{0}'.format(
+            self.training_omic_list))
 
-        print('#### report of assigned cluster:')
-        for key, value in Counter(self.test_labels).items():
-            print('class: {0}, number of samples :{1}'.format(key, value))
+        if self.cluster_method == 'kmeans':
+            self.clustering = KMeans(n_clusters=self.nb_clusters, n_init=100)
+
+        elif self.cluster_method == 'mixture':
+            self.clustering = GaussianMixture(
+                n_components=self.nb_clusters,
+                **MIXTURE_PARAMS
+            )
+
+        if not self.activities_train.any():
+            raise Exception('No components linked to survival!'\
+                            ' cannot perform clustering')
+        self._predict_best_k_for_cluster()
+
+        self.clustering.fit(self.activities_train)
+        labels = self.clustering.predict(self.activities_train)
+
+        labels = self._order_labels_according_to_survival(labels)
+        self.labels = labels
+
+        print("clustering done, labels ordered according to survival:")
+        for key, value in Counter(labels).items():
+            print('cluster label: {0}\t number of samples:{1}'.format(key, value))
+
+        print('\n')
+
+        nbdays, isdead = self.dataset.survival.T.tolist()
+        pvalue = coxph(self.labels, isdead, nbdays)
+        print('Cox-PH p-value (Log-Rank) for the cluster labels: {0}'.format(pvalue))
+
+        return self.labels
+
+    def look_for_survival_nodes(self, keys=None):
+        """
+        detect nodes from the autoencoder significantly
+        linked with survival through coxph regression
+        """
+        self.training_omic_list = []
+
+        if not keys:
+            keys = self.encoder_array.keys()
+
+        for key in keys:
+            self.training_omic_list.append(key)
+
+            encoder = self.encoder_array[key]
+            matrix_train = self.matrix_array_train[key]
+
+            activities = encoder.predict(matrix_train)
+
+            valid_node_ids = self._look_for_survival_nodes(activities)
+            self.valid_node_ids_array[key] = valid_node_ids
+
+            print('number of components linked to survival found:{0} for key {1}'.format(
+                len(valid_node_ids), key))
+
+            self.activities_array[key] = activities.T[valid_node_ids].T
+
+        self.activities_train = hstack([self.activities_array[key]
+                                        for key in self.activities_array])
+
+    def _predict_test_labels(self):
+        """ """
+        self.test_labels = self.classifier.predict(self.activities_test)
+        self.test_labels_proba = self.classifier.predict_proba(self.activities_test)
 
     def _predict_best_k_for_cluster(self):
         """ """
@@ -270,46 +318,6 @@ class SimDeep(DeepBase):
         else:
             self.clustering.set_params(n_clusters=best_k)
 
-    def predict_labels(self):
-        """
-        predict labels from training set
-        using K-Means algorithm on the node activities,
-        using only nodes linked to survival
-        """
-        print('performing clustering on the omic model with the following key:{0}'.format(
-            self.training_omic_list))
-
-        if self.cluster_method == 'kmeans':
-            self.clustering = KMeans(n_clusters=self.nb_clusters, n_init=100)
-
-        elif self.cluster_method == 'mixture':
-            self.clustering = GaussianMixture(
-                n_components=self.nb_clusters,
-                **MIXTURE_PARAMS
-            )
-
-        if not self.activities_train.any():
-            raise Exception('No components linked to survival!'\
-                            ' cannot perform clustering')
-        self._predict_best_k_for_cluster()
-
-        self.clustering.fit(self.activities_train)
-        labels = self.clustering.predict(self.activities_train)
-
-        labels = self._order_labels_according_to_survival(labels)
-
-        print("clustering done, labels ordered according to survival:")
-        for key, value in Counter(labels).items():
-            print('cluster label: {0}\t number of samples:{1}'.format(key, value))
-
-        print('\n')
-
-        nbdays, isdead = self.dataset.survival.T.tolist()
-        pvalue = coxph(labels, isdead, nbdays)
-        print('Cox-PH p-value (Log-Rank) for the cluster labels: {0}'.format(pvalue))
-
-        return labels
-
     def _order_labels_according_to_survival(self, labels):
         """
         Order cluster labels according to survival
@@ -335,35 +343,6 @@ class SimDeep(DeepBase):
             labels[labels_old == old_label] = self._label_ordered_dict[old_label]
 
         return labels
-
-    def look_for_survival_nodes(self, keys=None):
-        """
-        detect nodes from the autoencoder significantly
-        linked with survival through coxph regression
-        """
-        self.training_omic_list = []
-
-        if not keys:
-            keys = self.encoder_array.keys()
-
-        for key in keys:
-            self.training_omic_list.append(key)
-
-            encoder = self.encoder_array[key]
-            matrix_train = self.matrix_array_train[key]
-
-            activities = encoder.predict(matrix_train)
-
-            valid_node_ids = self._look_for_survival_nodes(activities)
-            self.valid_node_ids_array[key] = valid_node_ids
-
-            print('number of components linked to survival found:{0} for key {1}'.format(
-                len(valid_node_ids), key))
-
-            self.activities_array[key] = activities.T[valid_node_ids].T
-
-        self.activities_train = hstack([self.activities_array[key]
-                                        for key in self.activities_array])
 
     def _look_for_survival_nodes(self, activities):
         """

simdeep/survival_utils.py
@@ -36,6 +36,7 @@ class RankNorm():
 
         return np.matrix(X)
 
+
 class CorrelationReducer():
     def __init__(self):
         self.dataset = None
@@ -59,7 +60,12 @@ def load_survival_file(f_name, path_data=PATH_DATA, sep='\t'):
     """ """
     survival = {}
     f_surv = open(path_data + f_name, 'r')
-    f_surv.readline()
+    first_line = f_surv.readline()
+    first_split = first_line.split(sep)[2]
+
+    if first_split[2].isdigit() or first_split[1].isdigit():
+        print('warning! the first line of the survival' \
+              'file might be not a header!:\nfirst line:{0}'.format(first_line))
 
     for line in f_surv:
         ids, ndays, isdead = line.split(sep)[:3]